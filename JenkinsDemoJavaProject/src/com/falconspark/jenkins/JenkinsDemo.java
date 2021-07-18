87069212840a2746da9a652ce43566d008459f06
Murshid Khan <koolkhan2006@gmail.com>
2021-07-18 22:07:42 +0530

    Initial Commit

---
+++ JenkinsDemoJavaProject/src/com/falconspark/jenkins/JenkinsDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.falconspark.jenkins;
+
+/**
+ * @author Murshid
+ *
+ */
+public class JenkinsDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test Jenkins Demo");
+
+	}
+
+}
